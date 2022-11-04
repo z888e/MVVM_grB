0d6a7ae3f62e8ce59b222a454f80f2c6a1387709
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2022-11-04 12:28:17 +0100

    Create FilterView

---
+++ Eat_it/Eat_it/View/FilterView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterView.swift
+//  Eat_it
+//
+//  Created by Tatiana Simmer on 04/11/2022.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterView()
+    }
+}
