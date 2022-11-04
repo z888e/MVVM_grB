d250c5af464c05334ea40a707ab4af58e00659f6
z888e <zoemhrt@gmail.com>
2022-11-04 14:16:58 +0100

    vm error handling

--- Eat_it/Eat_it/ViewModel/RecipeViewModel.swift
+++ Eat_it/Eat_it/ViewModel/RecipeViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import Foundation
+import SwiftUI
+
+enum ErrorMessage: Error {
+    case noMessage
+    case decodingError
+    case badURL
+    case badResponse
+    
+    var localizedDescription: String {
+        switch self {
+            case .noMessage:
+                return "No message"
+            case .decodingError:
+                return "Error while decoding error"
+            case .badURL:
+                return "Missing URL"
+            case .badResponse:
+                return "Bad response"
+        }
+    }
+}
+
+class ViewModel: ObservableObject {
+    
+}
