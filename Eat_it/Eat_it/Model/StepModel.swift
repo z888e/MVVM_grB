d304a1e47a8e1dc876ef2981cf9d6c928756b48d
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2022-11-04 14:07:27 +0100

    added Models(User, Step, Ustensil)

---
+++ Eat_it/Eat_it/Model/StepModel.swift
@@ -0,0 +1,14 @@
+//
+//  StepModel.swift
+//  Eat_it
+//
+//  Created by Tatiana Simmer on 04/11/2022.
+//
+
+import Foundation
+
+struct Step: Codable {
+    var time: Date
+    var description: String?
+}
+
---
+++ Eat_it/Eat_it/Model/UstensilModel.swift
@@ -0,0 +1,12 @@
+//
+//  Ustensil Model.swift
+//  Eat_it
+//
+//  Created by Tatiana Simmer on 04/11/2022.
+//
+
+import Foundation
+
+struct Ustensil: Codable {
+    var name: String?
+}
