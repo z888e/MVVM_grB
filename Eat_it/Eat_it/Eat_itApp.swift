426285c5b6782243d4b321b01494e84c86b38750
z888e <zoemhrt@gmail.com>
2022-11-04 12:04:22 +0100

    init

---
+++ Eat_it/Eat_it/Eat_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eat_itApp.swift
+//  Eat_it
+//
+//  Created by Zoé Hartman on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Eat_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
